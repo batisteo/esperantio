29b82a4ac854f2110e41887917002e6ebef50e2d
Baptiste Darthenay <baptiste.darthenay@gmail.com>
2013-12-30 21:23:17 +0100

    Put JS out of evento_list template.

---
+++ static/js/mapo.js
@@ -0,0 +1,20 @@
+$(function() {
+
+    var map = L.map('mapo').setView([45.1, 3.9], 2);
+    var tiles = 'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+    L.tileLayer(tiles, {
+        attribution: 'Mapaj datumoj &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> kontribuantoj',
+        maxZoom: 19
+    }).addTo(map);
+    
+    $(".evento").each(function(){
+       var nomo = $(this).children(".nomo").text();
+       var urbo = $(this).children(".urbo").text();
+       var lat = $(this).children(".lat").text();
+       var long = $(this).children(".long").text();
+       var marker = L.marker([lat, long]).addTo(map);
+       var msg = "<strong>"+ nomo +"</strong><br/>"+ urbo;
+       marker.bindPopup(msg);
+    });
+        
+});
