e872cd64b96068a75ba3c98dd8c775f2c79133c9
Baptiste Darthenay <baptiste.darthenay@gmail.com>
2014-02-12 20:44:21 +0100

    URL refactoring.

--- esperantio/urls.py
+++ esperantio/urls.py
@@ -1,8 +1,5 @@
 from django.conf.urls import patterns, include, url
 
-from evento.views import HybridDetailView
-from evento.models import Evento
-
 from django.contrib import admin
 admin.autodiscover()
 
@@ -10,6 +7,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'esperantio.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
+    url(r'^kal/', include('evento.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 
@@ -23,41 +22,4 @@ urlpatterns = patterns('',
 )
 
 
-urlpatterns += patterns('evento.views',
-    url(r'^kal/arangxo/$',
-            view='arangxo_list',
-            name='arangxo_list',),
 
-    url(r'^kal/arangxo/aldoni/$',
-            view='arangxo_create',
-            name='arangxo_create',),
-
-    url(r'^kal/arangxo/(?P<pk>\d+)/$',
-            view='arangxo_detail',
-            name='arangxo_detail',),
-
-    url(r'^kal/arangxo/(?P<pk>\d+)/redakti/$',
-            view='arangxo_update',
-            name='arangxo_update',),
-
-    url(r'^kal/evento/$',
-            view='evento_list',
-            name='evento_list',),
-
-    url(r'^kal/arangxo/(?P<pk>\d+)/evento/aldoni/$',
-            view='evento_create',
-            name='evento_create',),
-
-    url(r'^kal/evento/(?P<pk>\d+)/$',
-            view=HybridDetailView.as_view(model=Evento),
-            name='evento_detail',),
-
-    url(r'^kal/evento/(?P<pk>\d+)/$',
-            view='evento_detail',
-            name='evento_detail',),
-
-    url(r'^kal/evento/(?P<pk>\d+)/redakti/$',
-            view='evento_update',
-            name='evento_update',),
-
-)
