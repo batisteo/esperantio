f263b27153a2aba5ec17effca423ea5716fb0e03
Baptiste Darthenay <baptiste.darthenay@gmail.com>
2014-02-22 01:27:07 +0100

    Bon, je suis fatigu? l?.

--- esperantio/settings/staging.py
+++ esperantio/settings/staging.py
@@ -7,15 +7,15 @@ TEMPLATE_DEBUG = DEBUG
 
 STATIC_ROOT = os.path.join(BASE_DIR, "..", "public", "static")
 
-ALLOWED_HOSTS = [c.ALLOWED_HOSTS),]
+ALLOWED_HOSTS = [c.ALLOWED_HOSTS,]
 
 DATABASES = {
     'default': {
-        'ENGINE': c.DATABASE_ENGINE),
-        'NAME': c.DATABASE_NAME),
-        'USER': c.DATABASE_USER),
-        'PASSWORD': c.DATABASE_PASSWORD),
-        'HOST': c.DATABASE_HOST),
-        'PORT': c.DATABASE_PORT),
+        'ENGINE': c.DATABASE_ENGINE,
+        'NAME': c.DATABASE_NAME,
+        'USER': c.DATABASE_USER,
+        'PASSWORD': c.DATABASE_PASSWORD,
+        'HOST': c.DATABASE_HOST,
+        'PORT': c.DATABASE_PORT,
     }
 }
