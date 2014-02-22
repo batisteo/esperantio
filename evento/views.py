0f359d3d64a6dfc5a513280ae52ce833a1034c87
Baptiste Darthenay <baptiste.darthenay@gmail.com>
2014-02-22 12:10:16 +0100

    Removed ajax views.

--- evento/views.py
+++ evento/views.py
@@ -1,30 +1,8 @@
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse
-from django.utils import json
 from django.views import generic
-from django.views.generic.detail import BaseDetailView, \
-    SingleObjectTemplateResponseMixin
 
 from .forms import ArangxoForm, EventoCreateForm,  EventoForm
 from .models import Arangxo, Evento
-
-
-class JSONResponseMixin(object):
-    def render_to_response(self, context):
-        return self.get_json_response(self.convert_context_to_json(context))
-    def get_json_response(self, content, **httpresponse_kwargs):
-        return HttpResponse(content, content_type='application/json', **httpresponse_kwargs)
-    def convert_context_to_json(self, context):
-        return json.dumps(context)
-
-
-class HybridDetailView(JSONResponseMixin, SingleObjectTemplateResponseMixin, BaseDetailView):
-    def render_to_response(self, context):
-        if self.request.is_ajax():
-            obj = context['object'].as_dict()
-            return JSONResponseMixin.render_to_response(self, obj)
-        else:
-            return SingleObjectTemplateResponseMixin.render_to_response(self, context)
 
 
 class ArangxoListView(generic.ListView):
@@ -42,7 +20,7 @@ arangxo_detail = ArangxoDetailView.as_view()
 class ArangxoCreateView(generic.CreateView):
     model = Arangxo
     form_class = ArangxoForm
-    
+
     def get_success_url(self):
         return reverse('arangxo_detail', args=[self.object.id])
 
@@ -52,7 +30,7 @@ arangxo_create = ArangxoCreateView.as_view()
 class ArangxoUpdateView(generic.UpdateView):
     model = Arangxo
     form_class = ArangxoForm
-    
+
     def get_success_url(self):
         return reverse('arangxo_detail', args=[self.object.id])
 
@@ -74,7 +52,7 @@ evento_detail = EventoDetailView.as_view()
 class EventoCreateView(generic.CreateView):
     model = Evento
     form_class = EventoCreateForm
-    
+
     def dispatch(self, request, *args, **kwargs):
         self.pk = kwargs['pk']
         return super(EventoCreateView, self).dispatch(request, *args, **kwargs)
@@ -93,7 +71,7 @@ evento_create = EventoCreateView.as_view()
 class EventoUpdateView(generic.UpdateView):
     model = Evento
     form_class = EventoForm
-    
+
     def get_success_url(self):
         return reverse('evento_detail', args=[self.object.id])
 
