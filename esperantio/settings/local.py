5951ffc0ab81072ea15459709c8acc10bc5b3218
Baptiste Darthenay <baptiste.darthenay@gmail.com>
2014-03-09 20:12:38 +0100

    Postgresql kiel loka datumbazo.

--- esperantio/settings/local.py
+++ esperantio/settings/local.py
@@ -13,7 +13,7 @@ INSTALLED_APPS += (
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': c.DATABASE_ENGINE,
+        'NAME': c.DATABASE_NAME,
     }
 }
