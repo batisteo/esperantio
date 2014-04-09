996794477eecca9a3fec6fbb7db18896160de0ea
Baptiste Darthenay <baptiste.darthenay@gmail.com>
2014-04-09 23:09:18 +0200

    Stilo kaj konduto de la dinamikaj menuoj.

--- esperantio/static/js/main.js
+++ esperantio/static/js/main.js
@@ -1,6 +1,16 @@
 $(function() {
-    $(".uzanto-nomo").click(function() {
-        $("header menu").toggle();
-        return false;
+    $(".uzanto").click(function(e) {
+        $("#uzanto-menuo").toggle();
+        e.preventDefault();
+        e.stopPropagation(); // for click outside
+    });
+    $("#lingvo").click(function(e) {
+        $("#lingvo-menuo").toggle();
+        e.preventDefault();
+        e.stopPropagation(); // for click outside
+    });
+    // Close menu if clicked outside opener
+    $(document).click(function(){
+        $(".dinamika-menuo").hide();
     });
 });
