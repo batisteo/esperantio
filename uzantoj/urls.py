16513a9f458b889c2a479c51252854eebf74b013
Baptiste Darthenay <baptiste@darthenay.fr>
2016-02-23 18:17:33 +0100

    Added change password form.

--- uzantoj/urls.py
+++ uzantoj/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login, logout
 
-from .views import uzanto_detail, uzanto_update, uzanto_create
+from .views import uzanto_detail, uzanto_update, uzanto_create, password_change
 
 
 urlpatterns = [
@@ -11,4 +11,5 @@ urlpatterns = [
     url(r'^$', view=uzanto_detail, name='uzanto_detail',),
     url(r'^redakti/$', view=uzanto_update, name='uzanto_update',),
     url(r'^nova/$', view=uzanto_create, name='uzanto_create',),
+    url(r'^pasvorto/nova/$', view=password_change, name='password_change'),
 ]
--- uzantoj/views.py
+++ uzantoj/views.py
@@ -1,17 +1,18 @@
+from django.conf import settings
 from django.views import generic
 from django.contrib.auth import login
+from django.contrib.auth.forms import PasswordChangeForm
 
 from braces.views import LoginRequiredMixin
 
-from esperantio.settings import base as settings
 from .models import Uzanto
 from .forms import CustomUserChangeForm, UzantoCreateForm
 
 
 class UzantoDetailView(LoginRequiredMixin, generic.DetailView):
     model = Uzanto
-    
-    def get_object (self):
+
+    def get_object(self):
         return self.request.user
 
 uzanto_detail = UzantoDetailView.as_view()
@@ -20,8 +21,8 @@ uzanto_detail = UzantoDetailView.as_view()
 class UzantoUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Uzanto
     form_class = CustomUserChangeForm
-    
-    def get_object (self):
+
+    def get_object(self):
         return self.request.user
 
 uzanto_update = UzantoUpdateView.as_view()
@@ -31,14 +32,32 @@ class UzantoCreateView(generic.CreateView):
     model = Uzanto
     form_class = UzantoCreateForm
     success_url = settings.LOGIN_REDIRECT_URL
-    
+
     def get(self, request, *args, **kwargs):
         self.request = request
         return super(UzantoCreateView, self).get(request, *args, **kwargs)
-    
+
     def form_valid(self, form, *args, **kwargs):
         form_valid = super(UzantoCreateView, self).form_valid(form, *args, **kwargs)
         login(self.request, self.object)
         return form_valid
 
 uzanto_create = UzantoCreateView.as_view()
+
+
+class PasswordChangeView(LoginRequiredMixin, generic.FormView):
+    model = Uzanto
+    form_class = PasswordChangeForm
+    success_url = settings.LOGIN_REDIRECT_URL
+    template_name = 'uzantoj/password_form.html'
+
+    def get_form_kwargs(self, **kwargs):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+password_change = PasswordChangeView.as_view()
